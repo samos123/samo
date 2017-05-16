004067d5b9e66d0e4498b1c1e43f53e396929abb
Sam Stoelinga <sammiestoel@gmail.com>
2017-05-15 21:37:46 -0700

    Add dummy code for detecting directory and video extensions

--- videowatcher.go
+++ videowatcher.go
@@ -5,26 +5,18 @@ import (
 	"github.com/fsnotify/fsnotify"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 type VideoWatcher struct {
 	*fsnotify.Watcher
-}
-
-func (watcher *VideoWatcher) ProcessEvents() {
-	for {
-		select {
-		case event := <-watcher.Events:
-			log.Println("event:", event)
-		case err := <-watcher.Errors:
-			log.Println("error:", err)
-		}
-	}
+	SupportedFormats []string
 }
 
 func NewVideoWatcher(dirs ...string) *VideoWatcher {
 	watcher, err := fsnotify.NewWatcher()
-	vw := &VideoWatcher{watcher}
+	vw := &VideoWatcher{watcher, []string{".mkv"}}
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -37,6 +29,42 @@ func NewVideoWatcher(dirs ...string) *VideoWatcher {
 		}
 	}
 	return vw
+}
+
+func (watcher *VideoWatcher) ProcessEvents() {
+	for {
+		select {
+		case event := <-watcher.Events:
+			log.Println("event:", event)
+			if event.Op&fsnotify.Create == fsnotify.Create {
+				if isDir(event.Name) {
+					log.Println("Adding subdirectory")
+					watcher.Add(event.Name)
+				} else if watcher.isSupportedFormat(event.Name) {
+					log.Println("Adding video")
+				}
+			}
+		case err := <-watcher.Errors:
+			log.Println("error:", err)
+		}
+	}
+}
+
+func isDir(path string) bool {
+	if stat, err := os.Stat(path); err == nil && stat.IsDir() {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (watcher *VideoWatcher) isSupportedFormat(path string) bool {
+	for _, format := range watcher.SupportedFormats {
+		if strings.HasSuffix(path, format) {
+			return true
+		}
+	}
+	return false
 }
 
 func DirAddHTTPHandler(w *VideoWatcher) http.Handler {
